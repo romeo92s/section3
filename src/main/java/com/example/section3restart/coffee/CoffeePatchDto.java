71654e9b30e139f08b39ce3fdef4854594513df9
이재헌 <romeo92s@naver.com>
2022-11-16 23:52:56 +0900

    coffee DTO실습

--- src/main/java/com/example/section3restart/coffee/CoffeePatchDto.java
+++ src/main/java/com/example/section3restart/coffee/CoffeePatchDto.java
@@ -1,12 +1,30 @@
 package com.example.section3restart.coffee;
 
 import lombok.AllArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @AllArgsConstructor
 public class CoffeePatchDto {
+
+    private long coffeeId;
+    @NotBlank
     public String korName;
+    @Pattern(regexp = "^([A-Za-z]\\s?)*$", message = "커피명(영문)은 영문이어야 합니다. 예) Cafe Latte")
+    @NotBlank
     public String engName;
+    @Range(min = 100, max =50000)
     public int price;
+
+    public long getCoffeeId() {
+        return coffeeId;
+    }
+
+    public void setCoffeeId(long coffeeId) {
+        this.coffeeId = coffeeId;
+    }
 
     public String getKorName() {
         return korName;
--- src/main/java/com/example/section3restart/coffee/CoffeePostDto.java
+++ src/main/java/com/example/section3restart/coffee/CoffeePostDto.java
@@ -1,12 +1,32 @@
 package com.example.section3restart.coffee;
 
 import lombok.AllArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 
 @AllArgsConstructor
 public class CoffeePostDto {
+
+    private long coffeeId;
+    @NotBlank
     private String korName;
+
+    @NotBlank
+    @Pattern(regexp = "^([A-Za-z])(\\s?[A-Za-z])*$", message = "커피명(영문)은 영문이어야 합니다. 예) Cafe Latte")
     private String engName;
+    @Range(min = 100, max = 50000)
     private int price;
+
+    public long getCoffeeId() {
+        return coffeeId;
+    }
+
+    public void setCoffeeId(long coffeeId) {
+        this.coffeeId = coffeeId;
+    }
 
     public String getKorName() {
         return korName;
